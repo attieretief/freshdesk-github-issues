d9b7a37e3b9dc907f13e1c4b0b7d61383b3c88aa
Attie Retief <attie@attieretief.com>
2023-12-21 10:35:03 +0200

    .

--- sync.py
+++ sync.py
@@ -9,7 +9,7 @@ github_token = os.environ.get("GITHUB_TOKEN")
 freshdesk_key = os.environ.get("FRESHDESK_KEY")
 freshdesk_url = os.environ.get("FRESHDESK_URL")
 org = os.environ.get("ORG")
-repo = os.environ.get("REPO").split('/')[1]
+language = os.environ.get("LANGUAGE")
 project_number = os.environ.get("PROJECT")
 status_field = os.environ.get("STATUS_FIELD")
 priority_field = os.environ.get("PRIORITY_FIELD")
@@ -41,9 +41,10 @@ def github_auth():
 
 
 def github_get_project_fields():
+    log.info("[yellow]Getting Github Project Fields")
     query = f"""
         {{
-        organization(login: "lincza"){{
+        organization(login: "{org}"){{
             projectV2(number: {project_number}) {{
             fields(first: 20) {{
                 nodes{{
@@ -55,6 +56,10 @@ def github_get_project_fields():
                     name
                     description
                     }}
+                }}
+                ...on ProjectV2Field{{
+                    id
+                    name
                 }}
                 }}
             }}
@@ -117,6 +122,33 @@ def github_get_members():
     else:
         log.error("[red]"+response.reason)
         log.error("[red]"+str(response.content))
+
+
+def github_get_repos():
+    log.info("[yellow]Getting Github Repositories")
+    url = f"https://api.github.com/orgs/{org}/repos"
+    auth = github_auth()
+    repos = []
+    morepages = True
+    while morepages:
+        response = requests.get(url=url,headers=auth)
+        if response.status_code==200:
+            content = json.loads(response.content)
+            for r in content:
+                if not r["archived"]:
+                    if language!='':
+                        if r["language"]=="AL":
+                            repos.append(r["name"])
+                    else:
+                        repos.append(r["name"])
+            try:
+                url = response.links["next"]["url"]
+            except:
+                morepages = False
+        else:
+            log.error("[red]"+response.reason)
+            log.error("[red]"+str(response.content))
+    return repos
 
 
 def github_get_project_cards():
@@ -226,7 +258,7 @@ def github_build_issue(ticket:dict):
     return issue
 
 
-def github_create_issue(ticket:dict):
+def github_create_issue(ticket:dict,repo:str):
     issue = github_build_issue(ticket)
     if issue!={}:
         log.info("[yellow]Creating Github Issue "+str(issue))
@@ -267,7 +299,7 @@ def github_compare_issue_field(gh_issue:dict,field:str,value:str,updated_issue:d
                 updated_issue.update({field: [value]})
 
 
-def github_update_issue(ticket:dict,gh_issue:dict):
+def github_update_issue(ticket:dict,gh_issue:dict,repo:str):
     title = f"{ticket["custom_fields"]["cf_development_task_title"]} (FD#{ticket["id"]})"
     body = f"<a href=https://{freshdesk_url}/a/tickets/{str(ticket['id'])}>Freshdeck Ticket #{str(ticket['id'])}</a>"
     label = map_type_label(ticket["type"])
@@ -292,7 +324,7 @@ def github_update_issue(ticket:dict,gh_issue:dict):
             log.error("[red]"+str(response.content))
 
 
-def github_get_issue(gh_issue_number:str):
+def github_get_issue(gh_issue_number:str,repo:str):
     log.info("[yellow]Getting Github Issue "+str(gh_issue_number))
     url = f"https://api.github.com/repos/{org}/{repo}/issues/{gh_issue_number}"
     auth = github_auth()
@@ -452,38 +484,27 @@ def freshdesk_update_field(field_id:int,field:dict):
         log.error("[red]"+str(response.content))
 
 
-def freshdesk_resolve_priority(priority:str):
-    fields = freshdesk_get_fields()
+def freshdesk_resolve_priority(priority:str,fields:dict):
     field_response = freshdesk_view_field(field_name='priority',fields=fields)
-    field_id = field_response["id"]
     field_choices = freshdesk_get_field_choices(response=field_response)
     return(next(ch for ch in field_choices if ch["value"] == priority)["label"])
 
 
-def freshdesk_get_ticket(ticket_id:int):
-    url = 'https://' + freshdesk_url + '/api/v2/search/tickets?query="cf_github_issue:' + "'" + str(gh_issue_number) + "'" + '"'
-    headers,auth = freshdesk_headers()
-    response = requests.get(url=url,headers=headers,auth=auth)
-    if response.status_code==200:
-        return(json.loads(response.content)["results"])
-    else:
-        log.error("[red]"+response.reason)
-        log.error("[red]"+str(response.content))
-
-
-def freshdesk_get_tickets():
+def freshdesk_get_tickets(repo:str):
     log.info("[yellow]Getting Freshdesk Tickets")
     url = 'https://' + freshdesk_url + '/api/v2/search/tickets?query="(status:<3 OR status:>6) AND tag:' + "'" + tag + "'" + ' AND cf_repository:' + "'" + repo + "'" + '"'
     headers,auth = freshdesk_headers()
     response = requests.get(url=url,headers=headers,auth=auth)
     if response.status_code==200:
-        return(json.loads(response.content)["results"])
+        tickets = json.loads(response.content)["results"]
+        log.info("[green]Freshdesk Tickets found: "+str(len(tickets)))
+        return(tickets)
     else:
         log.error("[red]"+response.reason)
         log.error("[red]"+str(response.content))
 
 
-def freshdesk_add_note(gh_issue:dict,ticket_id):
+def freshdesk_add_note(gh_issue:dict,ticket_id,repo:str):
     log.info("[yellow]Posting Freshdesk Note")
     try:
         assignee = gh_issue["user"]["login"]
@@ -532,7 +553,7 @@ def freshdesk_update_ticket_from_project(card:dict,ticket:dict):
             log.error("[red]"+str(response.content))
 
 
-def get_create_freshdesk_fields():
+def get_create_fields(repos:dict):
     fields = freshdesk_get_fields()
     github_project_fields = github_get_project_fields()
     
@@ -575,28 +596,6 @@ def get_create_freshdesk_fields():
             "displayed_to_customers": True
         }
         freshdesk_create_field(field=field)
-
-    if not next((field for field in fields if field["name"] == 'cf_repository'),False):
-        field={
-            "label": "Repository",
-            "label_for_customers": "Repository",
-            "type": "custom_dropdown",
-            "customers_can_edit": False,
-            "required_for_closure": False,
-            "required_for_agents": False,
-            "required_for_customers": False,
-            "displayed_to_customers": False
-        }
-        updated_field = freshdesk_add_field_choice(field=field)
-        field_response = freshdesk_create_field(updated_field)
-    else:
-        field_response = freshdesk_view_field(field_name='cf_repository',fields=fields)
-
-    field_id = field_response["id"]
-    field_choices = freshdesk_get_field_choices(response=field_response)
-    if not freshdesk_field_choice_exists(field_choices=field_choices,choice=repo):
-        updated_field = freshdesk_add_field_choice(field=field_response,field_choices=field_choices,new_value=repo)
-        freshdesk_update_field(field_id=field_id,field=updated_field)
 
     if not next((field for field in fields if field["name"] == 'cf_assigned_developer'),False):
         field={
@@ -647,29 +646,55 @@ def get_create_freshdesk_fields():
             updated_field = freshdesk_add_field_choice(field=field_response,field_choices=field_choices,new_value=status)
     if updated_field:
         freshdesk_update_field(field_id=field_id,field=updated_field)
-    
-    return fields,github_project_fields
+
+    if not next((field for field in fields if field["name"] == 'cf_repository'),False):
+        field={
+            "label": "Repository",
+            "label_for_customers": "Repository",
+            "type": "custom_dropdown",
+            "customers_can_edit": False,
+            "required_for_closure": False,
+            "required_for_agents": False,
+            "required_for_customers": False,
+            "displayed_to_customers": False
+        }
+        field_response = freshdesk_create_field(updated_field)
+    else:
+        field_response = freshdesk_view_field(field_name='cf_repository',fields=fields)
+
+    field_id = field_response["id"]
+    field_choices = freshdesk_get_field_choices(response=field_response)
+    for repo in repos:
+        if not freshdesk_field_choice_exists(field_choices=field_choices,choice=repo):
+            updated_field = freshdesk_add_field_choice(field=field_response,field_choices=field_choices,new_value=repo)
+            freshdesk_update_field(field_id=field_id,field=updated_field)
+
+    return fields, github_project_fields
 
 
-def create_update_github_issues(fd_fields:dict,gh_fields:dict):
-    tickets = freshdesk_get_tickets()
-    cards = github_get_project_cards()
+def create_update_github_issues(fd_fields,gh_fields:dict,repo:str,cards:dict):
+    log.info("[green]Starting sync for Repository "+repo)
+    tickets = freshdesk_get_tickets(repo)
     for t in tickets:
         if t["custom_fields"]["cf_github_issue"]==None:
             if (t["custom_fields"]["cf_development_task_title"]!=None) and (t["custom_fields"]["cf_repository"]!=None):
-                gh_issue = github_create_issue(t)
+                gh_issue = github_create_issue(t,repo)
                 if gh_issue!={}:
                     freshdesk_add_note(gh_issue=gh_issue,ticket_id=t["id"])
         else:
-            gh_issue = github_get_issue(t["custom_fields"]["cf_github_issue"])
+            gh_issue = github_get_issue(t["custom_fields"]["cf_github_issue"],repo)
             if gh_issue:
-                github_update_issue(t,gh_issue)
+                github_update_issue(t,gh_issue,repo)
                 card = next((c for c in cards if (c["issue_number"] == gh_issue["number"]) and (c["repository"] == repo)),False)
                 if card:
-                    github_update_project_card(card=card,company=freshdesk_get_company_name(ticket=t),priority=freshdesk_resolve_priority(t["priority"]),fields=gh_fields)
+                    github_update_project_card(card=card,company=freshdesk_get_company_name(ticket=t),priority=freshdesk_resolve_priority(t["priority"],fields=fd_fields),fields=gh_fields)
                     freshdesk_update_ticket_from_project(card=card,ticket=t)
+    log.info("[green]Ending sync for Repository "+repo)
 
 
 if __name__ == "__main__":
-    fd_fields, gh_fields = get_create_freshdesk_fields()
-    create_update_github_issues(fd_fields, gh_fields)
+    repos = github_get_repos()
+    cards = github_get_project_cards()
+    fd_fields, gh_fields = get_create_fields(repos)
+    for repo in repos:
+        create_update_github_issues(fd_fields,gh_fields,repo,cards)
